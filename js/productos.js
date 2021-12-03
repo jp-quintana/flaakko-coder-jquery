8de73dfa446096a669140078335424d2c360f043
jp-quintana <juanquintana1996@gmail.com>
2021-12-02 23:19:23 -0300

    Add class productos

--- js/productos.js
+++ js/productos.js
@@ -1,7 +1,33 @@
-const URL_JSON = "../productos.json";
+const URL_JSON = ""
+
+class Productos {
+  async getProductos(url) {
+    const result = await $.ajax({
+        method: "GET",
+        datatype: 'json',
+        url: url,
+        success: (response) => {
+          let productos = response;
+          return productos;
+        }
+    }).fail(() => {
+      throw new Error('error')
+    })
+    return result;
+  }
+}
+
+
+
+const productos = new Productos();
 
 $(document).ready(() => {
-  $.getJSON(URL_JSON, (response) => {
+  productos.getProductos("../productos.json").then(data => console.log(data))
+})
+
+
+// $(document).ready(() => {
+  // $.getJSON(URL_JSON, (response) => {
     // for (let i = 0; i < response.length; i++) {
     //   for (let j = 0; j < response[i].length; j++) {
     //     $('#productos').append(
@@ -23,31 +49,31 @@ $(document).ready(() => {
     //   }
     // }
 
-
-    for (let articulo of response) {
-      $('#productos').append(
-        `
-        <div class="productos__item">
-            <a href="producto.html" class="productos__enlace">
-              <div class="productos__imagen-contenedor">
-                <img src=${articulo.img1} alt="" class="productos__imagen">
-                <img src=${articulo.img2} alt="" class="productos__imagen-bottom">
-              </div>
-              <ul class="productos__info">
-                <li class="productos__nombre">${articulo.tipo} <span>${articulo.modelo}</span> ${articulo.color}</li>
-                <li class="productos__precio">$${articulo.precio}</li>
-              </ul>
-            </a>
-        </div>
-        `
-      )
-    }
-    // console.log(response[1].img1);
-    // console.log(response[2]);
-  }).fail(() => {
-    throw new Error('error')
-  })
-})
+//
+//     for (let articulo of response) {
+//       $('#productos').append(
+//         `
+//         <div class="productos__item">
+//             <a href="producto.html" class="productos__enlace">
+//               <div class="productos__imagen-contenedor">
+//                 <img src=${articulo.img1} alt="" class="productos__imagen">
+//                 <img src=${articulo.img2} alt="" class="productos__imagen-bottom">
+//               </div>
+//               <ul class="productos__info">
+//                 <li class="productos__nombre">${articulo.tipo} <span>${articulo.modelo}</span> ${articulo.color}</li>
+//                 <li class="productos__precio">$${articulo.precio}</li>
+//               </ul>
+//             </a>
+//         </div>
+//         `
+//       )
+//     }
+//     // console.log(response[1].img1);
+//     // console.log(response[2]);
+//   }).fail(() => {
+//     throw new Error('error')
+//   })
+// })
 
 
 // $(document).ready(() => {
