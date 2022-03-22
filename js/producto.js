4bd70cd9fbb570a1e5114b2329b86f5c5d68d9c7
jp-quintana <juanquintana1996@gmail.com>
2022-03-22 14:47:15 -0300

    prueba

---
+++ js/producto.js
@@ -0,0 +1,24 @@
+// let sku = 1000
+//
+// // Escuchar el evento submit
+// $('#form-producto').on('submit', () => {
+//   sku = sku + 1
+//   const tipo = "Buzo"
+//   const modelo = "De Gira"
+//   const color = "Blanco"
+//   const talle = $('#talle-producto').val()
+//   const precio = 6400
+//
+//   const producto = new ProductoCarrito(sku, tipo, modelo, color, talle, precio);
+//
+//   addCarrito(producto);
+// })
+//
+// // Mostar "Producto agregado con exito" en el browser
+// $('#form-producto').on('submit', () => {
+//   event.preventDefault();
+//   $('#exito-prodcuto').hide()
+//   $('#exito-producto').fadeIn('slow', () => {
+//     $('#exito-producto').fadeOut(3000)
+//   })
+// })
